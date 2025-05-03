fe486317279b773d65d00cd6cf36d5b7ead34a4a
CaDIxX <caio.delgado@aluno.faculdadeimpacta.com.br>
2025-05-03 18:12:34 -0300

    Update gol.py

--- gol.py
+++ gol.py
@@ -1,6 +1,6 @@
 import random
 elenco = ['centroavante','ponta-esquerda', 'ponta-direita','meia-atacante','meia-central','volante','lateral-esquerdo','lateral-direito','zagueiro-esquerda','zagueiro-direita','goleiro']
-quem_fez_gol = [0.4, 0.25,0.25,0.2,0.1,0.05,0.05,0.05,0.02,0.02,0.001]
+quem_fez_gol = [0.4, 0.25,0.25,0.2,0.1,0.05,0.05,0.05,0.02,0.02,0.001] # chances de cada posição fazer um gol
 
 placar1 = 0 # Placares dos respectivos times 
 placar2 = 0
@@ -10,7 +10,7 @@ for _ in range(11): # Repetir por Onze vezes se tando a escolha do time mandante
     teve_gol2 = random.choices([True, False], weights=[0.5,0.5], k=1)[0]
     teve_gol_ad = random.choices([True, False], weights=[0.5,0.5], k=1)[0]
     if teve_gol2:
-        jogador_gol = random.choices(elenco, weights=quem_fez_gol, k=1)[0]
+        jogador_gol = random.choices(elenco, weights=quem_fez_gol, k=1)[0] 
         placar1 += 1
         print(random.choices(elenco, weights=quem_fez_gol, k=1)[0])
     if teve_gol_ad:
@@ -18,7 +18,7 @@ for _ in range(11): # Repetir por Onze vezes se tando a escolha do time mandante
         placar2 += 1
         print(random.choices(elenco, weights=quem_fez_gol, k=1)[0])
 
-print(f'Placar final: {placar1}x{placar2}')
+print(f'Placar final: {placar1}x{placar2}') # Mostra placar final
             
 
 
